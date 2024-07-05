cda61f138de4a542cc3f5218812c38c41baf446c
retsch <r375ch@github>
2024-07-05 14:15:48 +0200

    new package structure, more unit tests

---
+++ src/absolute-humidity.js
@@ -0,0 +1,100 @@
+
+module.exports = function(RED) {
+
+    function AbsoluteHumidityNode(config) {
+	
+        RED.nodes.createNode(this,config);
+
+	context = this.context();
+	
+        this.on('input', function(msg, send, done) {
+	    
+	    // --------------------------------------------------------------
+	    // persist data
+	    //
+	    
+	    // the incoming message shall either have the property 'relativeHumidity'...
+	    if (typeof msg.relativeHumidity == 'number')
+		context.set('r',msg.relativeHumidity);
+	    else
+	    {
+		// ...or the appropriate topic and payload
+		//console.log(typeof msg.payload);
+		if (msg.topic === 'relativeHumidity' && typeof msg.payload === 'number')
+		    context.set('r',msg.payload);
+	    }
+
+	    // same for the temperature
+	    if (typeof msg.temperature == 'number')
+		context.set('T',msg.temperature);
+	    else
+	    {
+		if (msg.topic === 'temperature' && typeof msg.payload === 'number')
+		    context.set('T',msg.payload);
+	    }
+	    
+	    // --------------------------------------------------------------
+	    // get input
+	    //
+	    
+	    // relative Luftfeuchte [%]
+	    r = context.get('r');
+	    
+	    // Temperatur [°C]
+	    T = context.get('T');
+
+	    //console.log("r=" + r + ", T=" + T);
+
+	    const statustext = "rel.hum=" + r + ", temp=" + T;
+
+	    if (typeof r === 'undefined' || typeof T === 'undefined') {
+		this.status({fill:"red",shape:"dot",text:statustext});
+		done();
+		return;
+	    }
+	    
+	    this.status({fill:"green",shape:"dot",text:statustext});
+	    
+	    // --------------------------------------------------------------
+	    // calculation from https://www.wetterochs.de/wetter/feuchte.html
+	    //
+	    
+	    // Temperatur [K]
+	    TK  = T + 273.15;
+	    
+	    if (T >= 0) {
+		a   = 7.5; b = 237.3;	// Parameter bei T >= 0
+	    } else {
+		a   = 7.6; b = 240.7;	// Parameter bei T < 0 über Wasser
+	    }
+	    
+	    // SDD: Sättigungsdampfdruck [hPa]
+	    SDD = 6.1078 * Math.pow(10,a*T/(b+T));
+
+	    // DD:  augenblicklicher Dampfdruck [hPa]	   
+	    DD  = r/100 * SDD;
+
+	    // TD: Taupunkttemperatur [°C]
+	    v   = Math.log10(DD/6.1078);
+	    TD  = b*v/(a-v);
+
+	    // AF = absolute Feuchte [g/m³]
+	    AF  = Math.pow(10,5) * 18.016/8314.3 * DD/TK;	    
+
+	    TD = Math.round(TD*10)/10.0;
+	    AF = Math.round(AF*10)/10.0;
+	    
+	    msg.dewPoint = TD
+	    msg.absoluteHumidity = AF
+	    
+	    //console.log("msg.absoluteHumidity=" + msg.absoluteHumidity + ", msg.dewPoint=" + msg.dewPoint);
+
+	    descr = "abs.hum" + AF + "g/m³, dp=" + TD + "%";
+	    this.status({fill:"green",shape:"dot",text:descr});
+
+	    send(msg);
+        });
+    }
+    
+    RED.nodes.registerType("absolute-humidity",AbsoluteHumidityNode);
+}
--- test/_spec.js
+++ test/_spec.js
@@ -1,6 +1,9 @@
 var should = require("should");
 var helper = require("node-red-node-test-helper");
-var hummyNode = require("../absolute-humidity.js");
+var hummyNode = require("../src/absolute-humidity.js");
+
+var flow = [{ id: "n1", type: "absolute-humidity", name: "test name",wires:[["n2"]] },
+	    { id: "n2", type: "helper" }];
 
 //helper.init(require.resolve('node-red'));
 
@@ -11,21 +14,21 @@ describe('absolute-humidity Node', function () {
     });
     
     it('should be loaded', function (done) {
-	var flow = [{ id: "n1", type: "absolute-humidity", name: "test name" }];
-	helper.load(hummyNode, flow, function () {
+	helper.load(hummyNode, [{ id: "n1", type: "absolute-humidity", name: "test name" }], function () {
 	    var n1 = helper.getNode("n1");
 	    n1.should.have.property('name', 'test name');
 	    done();
 	});
     });
     
-    it('should make payload lower case', function (done) {
-	var flow =
-	    [{ id: "n1", type: "absolute-humidity", name: "test name",wires:[["n2"]] },
-	     { id: "n2", type: "helper" }];
+    
+    it('should create correct result for T=20°C  and r=75%', function (done) {
+	
 	helper.load(hummyNode, flow, function () {
+	    
 	    var n2 = helper.getNode("n2");
 	    var n1 = helper.getNode("n1");
+
 	    n2.on("input", function (msg) {
 		try {
 		    msg.should.have.property('dewPoint', 15.4);
@@ -38,6 +41,104 @@ describe('absolute-humidity Node', function () {
 	    n1.receive({ temperature: 20.0, relativeHumidity: 75 });
 	});
     });
+
+    it('should create correct result for T=10°C  and r=20%', function (done) {
+	
+	helper.load(hummyNode, flow, function () {
+	    
+	    var n2 = helper.getNode("n2");
+	    var n1 = helper.getNode("n1");
+
+	    n2.on("input", function (msg) {
+		try {
+		    msg.should.have.property('dewPoint', -11.9);
+		    msg.should.have.property('absoluteHumidity', 1.9);
+		    done();
+		} catch(err) {
+		    console.log("FAIL: " + err);
+		}		    
+	    });
+	    n1.receive({ temperature: 10.0, relativeHumidity: 20 });
+	});
+    });
     
+    it('should create correct result for T=0°C   and r=100%', function (done) {
+	
+	helper.load(hummyNode, flow, function () {
+	    
+	    var n2 = helper.getNode("n2");
+	    var n1 = helper.getNode("n1");
+
+	    n2.on("input", function (msg) {
+		try {
+		    msg.should.have.property('dewPoint', 0);
+		    msg.should.have.property('absoluteHumidity', 4.8);
+		    done();
+		} catch(err) {
+		    console.log("FAIL: " + err);
+		}		    
+	    });
+	    n1.receive({ temperature: 0, relativeHumidity: 100 });
+	});
+    });
+
+    it('should create correct result for T=-10°C and r=50%', function (done) {
+	
+	helper.load(hummyNode, flow, function () {
+	    
+	    var n2 = helper.getNode("n2");
+	    var n1 = helper.getNode("n1");
+
+	    n2.on("input", function (msg) {
+		try {
+		    msg.should.have.property('dewPoint', -18.4);
+		    msg.should.have.property('absoluteHumidity', 1.2);
+		    done();
+		} catch(err) {
+		    console.log("FAIL: " + err);
+		}		    
+	    });
+	    n1.receive({ temperature: -10, relativeHumidity: 50 });
+	});
+    });
+
+    it('should create correct results with two separate input messages', function (done) {
+	helper.load(hummyNode, flow, function () {
+	    var n2 = helper.getNode("n2");
+	    var n1 = helper.getNode("n1");
+	    n2.on("input", function (msg) {
+		try {
+		    msg.should.have.property('dewPoint');
+		    msg.should.have.property('absoluteHumidity');
+		    done();
+		} catch(err) {
+		    console.log("FAIL: " + err);
+		}		    
+	    });
+	    n1.receive({ relativeHumidity: 75 });
+	    n1.receive({ temperature: 20 });
+	});
+    });
+    
+
+    it('should create correct results with two separate input messages', function (done) {
+	helper.load(hummyNode, flow, function () {
+	    var n2 = helper.getNode("n2");
+	    var n1 = helper.getNode("n1");
+	    n2.on("input", function (msg) {
+		try {
+		    msg.should.have.property('dewPoint');
+		    msg.should.have.property('absoluteHumidity');
+		    done();
+		} catch(err) {
+		    console.log("FAIL: " + err);
+		}		    
+	    });
+	    n1.receive({ topic: 'relativeHumidity', payload: 75 });
+	    n1.receive({ topic: 'temperature',      payload: 20 });
+	});
+    });
+    
+
 });
 
